93a2f6d6212e04fa0be7755dabb0de5c6945a231
yeuxacucodon <chiendien2006@gmail.com>
2023-01-19 21:34:14 +0700

    update function move

--- jellyfish.js
+++ jellyfish.js
@@ -35,6 +35,7 @@ function canvas() {
 		this.springStiffness = 0.5;
 		this.isElastic = false;
 		this.viscosity = 0.1;
+        this.gravity = -1;
 	};
 
 	App.initialBirth = function () {
@@ -72,5 +73,44 @@ function canvas() {
 		this.target.radius = 10 + 20 * Math.sin(this.stepCount / 10);
 	};
 
-    App.move
+	App.move = function () {
+		this.target.x = this.xC + 150 * Math.sin(this.stepCount / 50);
+		this.target.y = this.yC + 150 * Math.cos(this.stepCount / 20);
+
+		for (var armIndex = 0; armIndex < this.arm.length; armIndex++) {
+			var arm = this.arms[armIndex];
+			var ownTargetAngle = (2 * Math.PI * armIndex) / this.arms.length;
+			var ownTarget = {
+				x: this.target.x + this.target.radius * Math.sin(ownTargetAngle),
+				y: this.target.y + this.target.radius * Math.cos(ownTargetAngle),
+			};
+			for (var i = 0; i < arm.length; i++) {
+				var p = arm[i];
+				var pLead = i === 0 ? ownTarget : arm[i - 1];
+				var angle = segmentAngleRad(p.x, p.y, pLead.x, pLead.y);
+				var dist = Math.sqrt(
+					Math.pow(p.y - pLead.y, 2) + Math.pow(p.y - pLead.y, 2),
+				);
+				var translationDist = dist - p.stickLength;
+				if (translationDist < 0) {
+					angle += Math.PI;
+					translationDist = Math.abs(translationDist);
+				}
+				var dx = translationDist * Math.sin(angle);
+				var dy = translationDist * Math.cos(angle);
+				if (!this.isElastic) {
+					p.x += dx;
+					p.y -= dy;
+				}
+                var xAcc = this.springStiffness * dx - this.viscosity * p.xSpeed;
+                var yAcc = this.springStiffness * dy + this.gravity - this.viscosity * p.ySpeed;
+                p.xSpeed += xAcc;
+                p.ySpeed -= yAcc;
+                p.x += 0.1 * p.xSpeed;
+                p.y += 0.1 * p.ySpeed;
+			}
+		}
+	};
 }
+
+canvas();
